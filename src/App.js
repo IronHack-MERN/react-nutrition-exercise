60a25af7f293d09fb0fbb8cc8218fc570b2cb3b2
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-13 18:31:41 +0200

    ADD Button component

--- src/App.js
+++ src/App.js
@@ -3,28 +3,52 @@ import "./App.css";
 import "bulma/css/bulma.css";
 import food from "./data/foods.json";
 import FoodBox from "./components/FoodBox";
+import Button from "./components/Button";
+
 
 class App extends Component {
   state = {
-    foods: { food },
+    foods: food.slice(0,3),
   };
+
+  xx = (food) => {
+    const oneFoods = {
+      name: "Papa rellena",
+      calories: 550,
+      image: "https://i.imgur.com/DupGBz5.jpg",
+      quantity: 2,
+    };
+    console.log("adding new food");
+    this.setState({
+      foods: [...this.state.foods, oneFoods]
+    });
+  };
+
+  addRandomFood = () => {
+    const randomFood = food[Math.floor(Math.random() * food.length)];
+    console.log('i add: ', randomFood)
+    this.setState({
+      foods: [...this.state.foods, randomFood],
+    })
+  }
 
   render() {
     return (
       <div className="App">
         <h1>NutritApp</h1>
+        <Button 
+         myProp={this.addRandomFood.bind(this.state,food)}
+        >
+          Add new Food
+          </Button>
         <div>
-          {
-            food.map( (food, index) => {
-              return(
-                <section key={index}>
-                <FoodBox
-                  {...food}
-                  />
-                </section>
-              )
-            })
-          }
+          {this.state.foods.map((food, index) => {
+            return (
+              <section key={index}>
+                <FoodBox {...food} />
+              </section>
+            );
+          })}
         </div>
       </div>
     );
---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Button = ( {children, myProp} ) => {
+    return ( 
+        <button onClick={myProp}>
+            {children}
+        </button>
+     );
+}
+ 
+export default Button;
