7231eea2f81ff96f36bba0e6ec1ba2a34ba520f8
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-13 13:29:35 +0200

    ADD FoodBox component and import to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css';
+import FoodBox from './components/FoodBox';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>NutritApp</h1>
+        <FoodBox/>
       </div>
     );
   }
