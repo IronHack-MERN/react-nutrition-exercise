1ad6f5dfebf8d43ff6034e5cb6bcc32cd7864fc1
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-16 23:44:48 +0200

    update form style

--- src/components/FormAddFood.js
+++ src/components/FormAddFood.js
@@ -1,46 +1,100 @@
 import React, { Component } from "react";
 
 class FormAddFood extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      kcal: '',
-      url: '',
+      name: "",
+      kcal: "",
+      url: "",
       showForm: false,
-    }
+    };
   }
 
   handleFormSubmit = (event) => {
     event.preventDefault();
     this.props.addNewFood(this.state);
     this.setState({
-      name: '',
-      calories: '',
-      image: '',
+      name: "",
+      calories: "",
+      image: "",
       showForm: false,
-    })
+    });
   };
 
-  handleChange(event){
+  handleChange(event) {
     let { name, value } = event.target;
-    this.setState({[name]: value})
+    this.setState({ [name]: value });
   }
 
   render() {
     return (
       <div>
         <form onSubmit={this.handleFormSubmit}>
-          <label>Name:</label>
-          <input type='text' name='name' value={this.state.name} onChange={e => this.handleChange(e)}/>
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label">Name:</label>
+            </div>
+            <div className="field-body">
+              <div className="field">
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    name="name"
+                    value={this.state.name}
+                    onChange={(e) => this.handleChange(e)}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
 
-          <label>Kc@l:</label>
-          <input type='text' name='calories' value={this.state.calories} onChange={e => this.handleChange(e)}/>
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label">Kc@l:</label>
+            </div>
+            <div className="field-body">
+              <div className="field">
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    name="calories"
+                    value={this.state.calories}
+                    onChange={(e) => this.handleChange(e)}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
 
-          <label>URL:</label>
-          <input type='text' name='image' placeholder="https://example.com" value={this.state.image} onChange={e => this.handleChange(e)}/>
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label">URL:</label>
+            </div>
+            <div className="field-body">
+              <div className="field">
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    name="image"
+                    placeholder="https://example.com"
+                    value={this.state.image}
+                    onChange={(e) => this.handleChange(e)}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
 
-          <input type="submit" value="ADD" />
+          <div className="level-right">
+            <div className='level-item'>
+            <button className="button is-primary">ADD</button>
+            </div>
+          </div>
+          
         </form>
       </div>
     );
