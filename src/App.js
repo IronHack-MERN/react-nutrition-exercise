92b6141e7c3e8c9ceb652a197a70be118b27ddaa
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-19 00:55:55 +0200

    ADD unordened list of tips

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import food from "./data/foods.json";
 import FoodBox from "./components/FoodBox";
 import Button from "./components/Button";
 import FormAddFood from "./components/FormAddFood";
-import SearchBar from './components/SearchBar';
+import SearchBar from "./components/SearchBar";
 
 class App extends Component {
   state = {
@@ -36,14 +36,14 @@ class App extends Component {
   };
 
   provaFunction = () => {
-    console.log('prova function');
-  } 
+    console.log("prova function");
+  };
 
   filterFood = (foodFilter) => {
-    const foodFiltered = food.filter( 
-      food => food.name.toLowerCase().includes(foodFilter)
-    )
-    
+    const foodFiltered = food.filter((food) =>
+      food.name.toLowerCase().includes(foodFilter)
+    );
+
     this.setState({
       foods: foodFiltered,
     });
@@ -53,9 +53,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="title is-1">NutritApp</h1>
-        <Button
-          myProp={this.addRandomFood.bind(this.state, food)}
-        >
+        <Button myProp={this.addRandomFood.bind(this.state, food)}>
           Add new random Food
         </Button>
 
@@ -66,33 +64,40 @@ class App extends Component {
         )}
 
         <div>
-          <SearchBar
-            filterFood={this.filterFood.bind(this.state)}
-          />
+          <SearchBar filterFood={this.filterFood.bind(this.state)} />
         </div>
         <div className="columns">
-        
           <div className="column">
             {this.state.foods.map((food, index) => {
               return (
                 <section key={index}>
-                  <FoodBox 
-                    {...food}
-                    provaFunction = {this.provaFunction}
-                   />
+                  <FoodBox {...food} provaFunction={this.provaFunction} />
                 </section>
               );
             })}
           </div>
 
           <div className="column">
-            <h2 className="title">ToDay Food</h2>
-            <ul>hola que ase</ul>
-            <p>Total of Calories </p>
+            <h2 className="title"> </h2>
+            <div className='container is-medium'>
+            <h4 className='title'>Consejos de alimentación:</h4>
+            <ul>
+              <li>Coma alimentos variados.</li>
+              <li>
+                Base su dieta en alimentos ricos en hidratos de carbono.
+              </li>
+              <li>Tome muchas frutas y verduras.</li>
+              <li>Mantenga un peso corporal saludable y siéntase bien.</li>
+              <li>
+                Coma raciones moderadas: reduzca, no elimine alimentos.
+              </li>
+              <li>Coma regularmente.</li>
+              <li>Beba muchos líquidos.</li>
+              <li>Muévase.</li>
+            </ul>
+            </div>
           </div>
         </div>
-
-      
       </div>
     );
   }
